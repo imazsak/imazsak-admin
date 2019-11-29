077ed18c4732bb95c9126b1cf1a53b6dfffa5b32
Ksisu <horkris@gmail.com>
2019-11-29 20:36:38 +0100

    Remove login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,20 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {TodoComponent} from './todo/todo.component';
-import {LoginComponent} from './login/login.component';
 
 
 const routes: Routes = [
-  { path: 'home', component: TodoComponent },
-  { path: 'groups', component: TodoComponent },
-  { path: 'users', component: TodoComponent },
-  { path: 'prayers', component: TodoComponent },
-  { path: 'feedback', component: TodoComponent },
-  { path: 'login', component: LoginComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {path: 'home', component: TodoComponent},
+  {path: 'groups', component: TodoComponent},
+  {path: 'users', component: TodoComponent},
+  {path: 'prayers', component: TodoComponent},
+  {path: 'feedback', component: TodoComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {LayoutModule} from '@angular/cdk/layout';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
-import { TodoComponent } from './todo/todo.component';
-import { LoginComponent } from './login/login.component';
+import {TodoComponent} from './todo/todo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoComponent,
-    LoginComponent,
   ],
   imports: [
     BrowserModule,
@@ -37,4 +35,5 @@ import { LoginComponent } from './login/login.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
